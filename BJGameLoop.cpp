13e70e4f235748c26a9d81ea4c2c08c5ceb85313
Paul Standley <paulstandley1972@gmail.com>
2020-07-12 22:05:52 +0100

    start game loops

--- BJGameLoop.cpp
+++ BJGameLoop.cpp
@@ -5,3 +5,47 @@
 #include <iostream>
 #include <random> // std::mt19937
 #include "BJHeader.h"
+
+
+bool playerWantsHit()
+{
+    while (true)
+    {
+        std::cout << "(h) to hit, or (s) to stand: ";
+
+        char ch{};
+        std::cin >> ch;
+
+        switch (ch)
+        {
+        case 'h':
+            return true;
+        case 's':
+            return false;
+        }
+    }
+}
+
+bool playerTurn(const deck_type& deck, index_type& nextIndex, Player& player)
+{// return true if player went bust else false
+    while (true)
+    {
+        std::cout << "You have: " << player.score << '\n';
+        if (player.score > maximumScore)
+        {// This can happen even before the player had a choice if they drew 2 aces.
+            return true;
+        }
+        else
+        {
+            if (playerWantsHit())
+            {
+                player.score += getCardValue(deck[nextIndex++]);
+            }
+            else
+            {// player ont bust
+                return false;
+            }
+        }
+    }
+}
+
--- BJHeader.h
+++ BJHeader.h
@@ -54,8 +54,9 @@ int getCardValue(const Card& card);
 
 void printDeck(const deck_type& deck);
 void shuffleDeck(deck_type& deck);
-
+// play game
 bool playerWantsHit();
+bool playerTurn(const deck_type& deck, index_type& nextIndex, Player& player);
 
 
 
